f2aa48a11c5a8f30edac6b17fca35386373da916
AlexsandrPROG-S-20 <98742758+AlexsandrPROG-S-20@users.noreply.github.com>
2023-09-04 22:19:03 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Include the necessary files
+require_once 'includes/functions.php';
+require_once 'templates/header.php';
+?>
+<main>
+    <div class="container">
+        <h1>Приветствую на сайте объявлений</h1>
+        <p>Пожалуйста <a href="register.php"style="color: blue;">зарегестрируйтесь</a> если вы не зарегестрированы, или <a href="login.php"style="color: blue;">авторизайтесь</a> что бы видеть объявления сайта</p>
+        <p>Перейдите на страничку <a href="ads.php" style="color: blue;">все объявления</a> что бы увидеть объявления</p>
+    </div>
+</main>
+
+<?php require_once 'templates/footer.php';
+ ?>
